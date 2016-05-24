6da671994fc04f7352a1cd34fc467cfe262c7d75
lburgazzoli <lburgazzoli@gmail.com>
2016-05-24 12:00:02 +0200

    remove camel-consul as merged in Apache Camel

--- camel-consul/src/main/bash/consul_client_install.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-git clone https://github.com/OrbitzWorldwide/consul-client.git /tmp/consul-client
-./mvnw -f /tmp/consul-client/pom.xml clean install -Dmaven.javadoc.skip=true -Dmaven.test.skip=true
